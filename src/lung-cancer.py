021427134f9edfb1f399dcb325f37c551f3d94f1
Jack Butler <jackapbutler@gmail.com>
2021-07-15 18:51:04 +0100

    Update lung-cancer.py

--- src/lung-cancer.py
+++ src/lung-cancer.py
@@ -5,7 +5,11 @@ import pandas as pd
 df = pd.read_csv(DATA_PATH)
 df = df.dropna(thresh=int(0.1*len(df)))
 df = df.drop(['target', 'BlindedIDs', 'PATIENT_ID'], axis=1)
+columns = list(df.columns)
 
-print('Column Names \n', list(df.columns))
+print('Column Names \n', columns)
 print('\n High Risk Counts \n', df['High_risk'].value_counts())
 
+PATIENT_COLS = ['Stratagene', 'DC_STUDY_ID', 'SITE', 'TESTTYPE', 'IN_DC_STUDY', 'GENDER', 'AGE_AT_DIAGNOSIS', 'RACE', 'ADJUVANT_CHEMO', 'ADJUVANT_RT', 'VITAL_STATUS', 'FIRST_PROGRESSION_OR_RELAPSE', 'MONTHS_TO_FIRST_PROGRESSION', 'MTHS_TO_LAST_CLINICAL_ASSESSMENT', 'MONTHS_TO_LAST_CONTACT_OR_DEATH', 'SMOKING_HISTORY', 'SURGICAL_MARGINS', 'PATHOLOGIC_N_STAGE', 'PATHOLOGIC_T_STAGE', 'MEDIAN_INTENSITY_UNNORMALIZED', 'PCT_ARRAY_OUTLIER', 'PCT_SINGLE_OUTLIER', 'WARNING', 'LABORATORY_BATCH', 'Histologic grade']
+TARGET_COL = 'High_risk'
+GENE_COLS = list(set(columns) - set(PATIENT_COLS) - set([TARGET_COL]))
